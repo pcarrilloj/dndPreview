07204e8c833b711b5fbd2341e9db0b70c49b0998
Pavel Carrillo <pavel.carrilloj@gmail.com>
2014-06-03 14:33:10 -0500

    xhr tested

--- src/dndpreview.js
+++ src/dndpreview.js
@@ -6,84 +6,165 @@
 
 /*jslint browser: true */
 /*jslint bitwise: true */
-/*global jQuery, FileReader*/
+/*global jQuery, FileReader, FormData*/
 
 //clousure
 (function ($) {
     'use strict';
     
     $.fn.dndPreview = function (options) {
-        
-        var defaults = {
-            holderSelector: '.holder',
+       
+        /**
+        * class private vars
+        */
+        var defaults = { //default configs
             acceptedTypes: {
                 'image/png': true,
                 'image/jpeg': true,
                 'image/gif': true
-            }
-        }, settings,
-            holder,
-            tests = {
-                filereader: (typeof FileReader !== 'undefined'),
-                dnd: document.createElement('span').hasOwnProperty('draggable')
             },
-            fileupload = document.getElementById('upload'),
-            $self = this;
-
-
-        function previewfile(event) {
-            var image = new Image();
-            image.src = event.target.result;
-            image.width = $(holder).width(); // a fake resize
-            while (holder.firstChild) { //remove previus if any
-                holder.removeChild(holder.firstChild);
+            fallback: false,
+            remote: '/devnull.php',
+            fileReaded: null, //functin callback
+            fileUploaded: null, //functin callback
+            textSendButton: "Subir imagen", //solo para el fallback
+            message:"arrastra y suelta una imagen al recuadro",
+            extraData:{} //json extra data to post
+        },
+            $self = this,//referencia de el contexto para las funciones internas 
+            settings, //actual config
+            holder, 
+            tests = { //tests for browser capabilities 
+                filereader: typeof FileReader !== 'undefined',
+                dnd: 'draggable' in document.createElement('span'),
+                formdata: !!window.FormData,
+                progress: new XMLHttpRequest().hasOwnProperty("upload")
+            };
+        
+        /**
+        * send file to the remote ulr
+        */
+        function upload(file) {
+            var formData, xhr;
+            if (!tests.formdata) {
+                return;
             }
-            holder.appendChild(image);
-        }
-
-        function readfiles(file) {
-            if (tests.filereader === true) {
-                if (settings.acceptedTypes[file.type] !== true) {
-                    return false;
+            formData = new FormData();
+            formData.append('file', file);
+            
+            xhr = new XMLHttpRequest();
+            xhr.open('POST', settings.remote);
+            xhr.on
+            xhr.onload = function () {
+                //progress.value = progress.innerHTML = 100;
+                if (typeof settings.fileUploaded === 'function') {
+                    settings.fileUploaded();
                 }
-                var reader = new FileReader();
-                reader.onload =  previewfile;
-                reader.readAsDataURL(file);
-            } else {
-                holder.innerHTML += '<p>Uploaded ' + file.name + ' ' + (file.size ? (file.size / 1024 | 0) + 'K' : '');
+            };
+            /*
+            if (tests.progress) {
+                xhr.upload.onprogress = function (event) {
+                    if (event.lengthComputable) {
+                        var complete = (event.loaded / event.total * 100 | 0);
+                        progress.value = progress.innerHTML = complete;
+                    }
+                };
+            }*/
+
+            xhr.send(formData);
+        }
+        /**
+        *Shows the image file inside the holder div
+        */
+        function previewfile(event) {
+            
+            var image = document.createElement('img'),
+                $holder = $self.find('.holder');
+            image.src = event.target.result;
+            image.width = $holder.width(); // a fake resize
+            $holder.empty(); //remove prev if any
+            $holder.append(image);
+            
+            if (typeof settings.fileReaded === 'function') {
+                settings.fileReaded();
             }
         }
         
+        /**
+        * creates a file reader and atach the 
+        * previewfile to the onload event of the reader
+        */
+        function readfiles(file) {
+            if (settings.acceptedTypes[file.type] !== true) {
+                return false;
+            }
+            var reader = new FileReader();
+            reader.onload =  previewfile;
+            reader.readAsDataURL(file);
+            upload(file);
+        }
+        /**
+        * creates the elemt holder 
+        * and atach handlers for drag&drop 
+        * events
+        */
+        function setupdnd() {
+            $self.html("<div><p>"+settings.message+"</p></div>");
+            $self.find("div").addClass('holder');
+            holder = $self.find(".holder").get(0);
+
+            holder.ondragover = function () {
+                this.className = 'holder hover';
+                return false;
+            };
+            holder.ondragend = function () {
+                this.className = 'holder';
+                return false;
+            };
+            holder.ondrop = function (e) {
+                this.className = 'holder';
+                e.preventDefault();
+                readfiles(e.dataTransfer.files[0]);
+            };
+        }
+        /**
+        * set up the fallback metod for old browsers
+        * a simple form with a input file and  send button
+        */
+        function setupfallback() {
+            var formtext;
+            
+            formtext = "<form action='" + settings.remote + "' method='post'>"
+            formtext += "<input type='file' id='file' name='subir archivo'/>"
+            /*$.each(settings.extraData,function(i,data){
+                
+            });*/
+            formtext += "<button name='name' value='value' type='submit'>"
+            formtext += settings.textSendButton 
+            formtext += "</button>"
+            
+            $self.html(formtext);
+        }
+        /**
+        * start point of the script, extends the setings
+        * test the capabilities of the browser for setup the dnd or 
+        * the fallback
+        */
         function ini() {
-            $self.html("<div class='holder'></div>");
+            
             // junta los defaults con las opciones
             settings = $.extend(defaults, options);
-            holder = $self.find(settings.holderSelector).get(0);
-            holder.className = 'holder';
-            //valida que tenga soporte para drang and drop
-            if (tests.dnd) {
-                holder.ondragover = function () {
-                    this.className = 'holder hover';
-                    return false;
-                };
-                holder.ondragend = function () {
-                    this.className = 'holder';
-                    return false;
-                };
-                holder.ondrop = function (e) {
-                    this.className = 'holder';
-                    e.preventDefault();
-                    readfiles(e.dataTransfer.files[0]);
-                };
-            } else {
-                fileupload.className = 'hidden';
-                fileupload.querySelector('input').onchange = function () {
-                    if (this.files > 0) {
-                        readfiles(this.files[0]);
-                    }
-                };
+            //valida que tenga soporte para drang and drop filereader y formdata para trabajar 
+            if (tests.dnd && tests.filereader && tests.formdata && settings.fallback === false) {
+                setupdnd();
+            } else { // en caso de no tener soporte a las apis necesarias hago un fallback a un form normal
+                setupfallback();
             }
         }
+        
+        /**
+        *bootstrap the script
+        */
         ini();
     };
 }(jQuery));
--- test/dndTest.js
+++ test/dndTest.js
@@ -5,11 +5,133 @@
 
 /*jslint browser: true */
 /*jslint bitwise: true */
-/*global module, form, test, pdp, $ */
+/*global module, form, test, asyncTest, start, $, equal, ok, Blob, sinon */
+
+
+function fireCustomEvent(eventName, element, data) {
+    'use strict';
+    var event;
+    data = data || {};
+    if (document.createEvent) {
+        event = document.createEvent("HTMLEvents");
+        event.initEvent(eventName, true, true);
+    } else {
+        event = document.createEventObject();
+        event.eventType = eventName;
+    }
+
+    event.eventName = eventName;
+    event = $.extend(event, data);
+
+    if (document.createEvent) {
+        element.dispatchEvent(event);
+    } else {
+        element.fireEvent("on" + event.eventType, event);
+    }
+}
 
 module("dndPreview", {
 });
 
 test("focustxtbox", function () {
-    $("#dnd").dndPreview();
+    'use strict';
+    function testDom() {
+        var actual;
+        $("#dnd").dndPreview();
+        equal($("#dnd > .holder").length, 1, "se esperaba un elemento con la clase holder");
+    }
+    testDom();
+});
+
+test("eventHandlers", function () {
+    'use strict';
+    function testDragEvents() {
+        $("#dnd").dndPreview();
+        equal($("#dnd > div").length, 1, "se esperaba un elemento con la clase holder");
+        fireCustomEvent('dragover', $("#dnd > div").get(0));
+        ok($("#dnd > div ").hasClass("holder"), "se esperaba tuviera la clase holder");
+        ok($("#dnd > div ").hasClass("hover"), "se esperaba tuviera la clase hover");
+        fireCustomEvent('dragend', $("#dnd > div").get(0));
+        ok($("#dnd > div ").hasClass("holder"), "se esperaba tuviera la clase holder");
+        ok(!$("#dnd > div ").hasClass("hover"), "se esperaba no tuviera la clase hover");
+    }
+    testDragEvents();
+});
+
+test("fallback", function () {
+    'use strict';
+    function testFormCreated() {
+        var config, $form;
+        config = {fallback : true, //force fallback
+                  remote: "/dev/null"
+                 };
+        $("#dnd").dndPreview(config);
+        equal($("#dnd > div").length, 0, "se no tuviera div");
+        equal($("#dnd > form ").length, 1, "se esperaba tuviera un elemento form");
+        $form = $("#dnd > form ");
+        equal($form.attr('action'), "/dev/null", "se esperaba un action = /dev/null");
+        equal($("#dnd > form > input").length, 1, "se esperaban 2 inputs en el form");
+        equal($("#dnd > form > Button").length, 1, "se esperaban 2 inputs en el form");
+        equal($("#dnd > form > :file").length, 1, "se esperaban 1 input file en el form");
+        equal($("#dnd > form > :submit").length, 1, "se esperaban 1 input submit en el form");
+        
+    }
+    testFormCreated();
+});
+
+module("dndPreviewServerComunication", {
+    setup: function() {
+        this.server = sinon.fakeServer.create();
+    },
+    teardown: function() {
+        this.server.restore();        
+        delete this.server;
+    }
+});
+
+asyncTest("previewOnDrop", function () {
+    'use strict';
+    expect(2);
+    var server = this.server,
+        config,
+        mockedfile;
+    config = {
+        remote: "/fileupload",
+        fileReaded: function () {
+            equal($('#dnd > div > img').length, 1, "se esperaba un elemento de imagen");
+            start();
+        }
+    };
+    $("#dnd").dndPreview(config);
+    
+    //uso un blob como un mock de file 
+    //refer http://www.nczonline.net/blog/2012/06/05/working-with-files-in-javascript-part-5-blobs/
+    mockedfile = new Blob(["Hello world!"], { type: "image/jpeg" });
+    equal(mockedfile.type, "image/jpeg");
+    fireCustomEvent('drop', $("#dnd > div").get(0), {dataTransfer: {files: [mockedfile]}});
+});
+
+test("xhrOndrop", function () {
+    'use strict';
+    var config,
+        mockedfile;
+    equal(this.server.requests.length, 0, "se esperaba que se hiciera un request");
+    this.server.respondWith("GET", "/fileupload",
+                   [200, { "Content-Type": "application/json" },
+                    '[{ id: 12, comment: "Hey there" }]']);
+    config = {
+        remote: "/fileupload"
+    };
+    $("#dnd").dndPreview(config);
+    
+    //uso un blob como un mock de file 
+    //refer http://www.nczonline.net/blog/2012/06/05/working-with-files-in-javascript-part-5-blobs/
+    mockedfile = new Blob(["Hello world!"], { type: "image/jpeg" });
+    equal(mockedfile.type, "image/jpeg");
+    //ref http://jsfiddle.net/pcarrilloj/M38ef/3/
+    equal(this.server.requests.length, 1, "se espera un request que se hace como test");
+    fireCustomEvent('drop', $("#dnd > div").get(0), {dataTransfer: {files: [mockedfile]}});
+    this.server.respond();
+    equal(this.server.requests.length, 2, "se esperaba que se hiciera un request real y uno de pruebas");
+    
 });
